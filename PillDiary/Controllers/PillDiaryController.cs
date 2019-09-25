9c699287c81be4fdc166f3fc20b357bab791578c
parekhp <parekhp@mskcc.org>
2019-09-25 11:13:15 -0400

    ajax refresh

--- PillDiary/Controllers/PillDiaryController.cs
+++ PillDiary/Controllers/PillDiaryController.cs
@@ -3,6 +3,7 @@ using PillDiary.Repository;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -11,12 +12,14 @@ namespace PillDiary.Controllers
     public class PillDiaryController : Controller
     {
         // GET: PillDiary
-        public ActionResult Index()
+        public async Task<ActionResult> Index()
         {
-            IPillRepository repo = new PillRepository();
-            var medata = repo.GetPatientMedications("12345678");
-            var reporting = repo.GetPatientPillReporting("12345678");
-            return View(new Patient() { MedicationInfos = medata, PillReportings = reporting });
+            //IPillRepository repo = new PillRepository();
+            //var medata = repo.GetPatientMedications("12345678");
+            //var reporting = repo.GetPatientPillReporting("12345678");
+            //return View(new Patient() { MedicationInfos = medata, PillReportings = reporting });
+            var model = await GetFullAndPartialViewModel();
+            return View(model);
         }
         //public ActionResult PillDiary()
         //{
@@ -25,18 +28,42 @@ namespace PillDiary.Controllers
         [HttpPost]
         public ActionResult Insert(PillReporting reporting)
         {
+            IPillRepository repo = new PillRepository();
             try
             {
-                IPillRepository repo = new PillRepository();
+               
                 if (reporting.PillTimeStamp.HasValue)
                     reporting.PillTimeStamp = DateTime.Now.Date + reporting.PillTimeStamp.Value.TimeOfDay;
                 repo.InsertPill(reporting);
+              
             }
             catch (Exception ex)
             {
                 //Log errror
             }
+
             return Json(new { status = "success" });
         }
+        private async Task<Patient> GetFullAndPartialViewModel()
+        {
+            IPillRepository repo = new PillRepository();
+            var medata = repo.GetPatientMedications("12345678");
+            var reporting = repo.GetPatientPillReporting("12345678");
+            return new Patient() { MedicationInfos = medata, PillReportings = reporting };
+        }
+
+        [HttpGet]
+        [OutputCache(Duration = 0)]
+        public async Task<ActionResult> GetPillReporting()
+        {
+            var model = await this.GetFullAndPartialViewModel();
+            return PartialView("_pillHistory", model);
+            // IPillRepository repo = new PillRepository();
+            // var medata = repo.GetPatientMedications("12345678");
+            // var reportingList = repo.GetPatientPillReporting("12345678");
+            //// return View();
+            // return PartialView("_pillHistory", new Patient() { MedicationInfos = medata, PillReportings = reportingList });
+        }
+
     }
 }
